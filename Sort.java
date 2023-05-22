5642d06e85ed1d6e59a1e2f89fc003a87a57db77
kulkarniomkar00 <kulkarniomkar767@gmail.com>
2023-05-22 02:37:09 -0700

    Added Bubble sort

---
+++ Sort.java
@@ -0,0 +1,27 @@
+class Sort{  
+    public static void main(String args[]){  
+    	int[] arr = {52, 26, 7, 95, 57, 8, 21, 93, 12, 47};
+	bubbleSort(arr, 10);
+	System.out.println("Bubble Sort");
+	printArr(arr, 10);
+    }
+
+    public static bubbleSort(int[] arr, int n 10) {
+    	int i, j;
+    	for (i = 0; i < n - 1; i++) {
+            for (j = 0; j < n - i - 1; j++) {
+            	if (arr[j] > arr[j + 1]) {
+		    int tmp = arr[j];
+		    arr[j] = arr[j+1];
+		    arr[j+1] = tmp;
+		}
+	    }
+	}
+    }
+
+    public static void printArr(int[] arr, int n 10) {
+	for(int i = 0; i < n; i++) {
+	    System.out.print(arr[i] + " ");
+	}
+    }
+}  
